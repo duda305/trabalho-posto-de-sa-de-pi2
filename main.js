2f981db508c5b0c11f91c3a44dbf5bc1168be2d3
malu196 <maria.costa.6@academico.ifpb.edu.br>
2025-05-05 11:45:05 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,1 @@
- // Loop para exibir médicos na página
-        const container = document.getElementById("lista-medicos");
 
-        medicosDisponiveis.forEach(medico => {
-            const div = document.createElement("div");
-            div.className = "medico";
-            div.innerHTML = `
-                <strong>Nome:</strong> ${medico.nome} <br>
-                <strong>Especialidade:</strong> ${medico.especialidade} <br>
-                <strong>Horário:</strong> ${medico.horario}
-            `;
-            container.appendChild(div);
-        });

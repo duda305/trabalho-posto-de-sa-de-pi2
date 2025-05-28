3010512aa352c958d79c75872e11619e9c6e009b
malu196 <maria.costa.6@academico.ifpb.edu.br>
2025-05-28 09:11:12 -0300

    Update medicos.js

--- projeto/medicos.js
+++ projeto/medicos.js
@@ -1,34 +1,34 @@
 export const medicos = [
     {
-        "id": "Id da Médica 1: 0001",
+        "id": "0001",
         "nome": "Dra. Luiza Costa",
         "especialidade": "Cardiologista",
         "descricao": "Descrição: Especialista em doenças do coração, pressão alta e arritmias.",
-        "horario": "Horário: 08h - 12h",
+        "horario": "08h - 12h",
         "foto": "https://img.freepik.com/fotos-gratis/bela-jovem-doutora-olhando-a-camera-no-escritorio_1301-7807.jpg?t=st=1746890201~exp=1746893801~hmac=cee08a011e4ccaf0c0d554066d435649fdb6d49ec41a1fdd30382c31926f5037&w=740"
     },
     {
-        "id": "Id da Médica 2: 0002",
+        "id": "0002",
         "nome": "Dra. Allana Damázio",
         "especialidade": "Dermatologista",
         "descricao": "Descrição: Cuida da saúde da pele, diagnóstico de câncer de pele e tratamentos dermatológicos.",
-        "horario": "Horário: 08h - 14h",
+        "horario": "08h - 14h",
         "foto": "https://img.freepik.com/fotos-gratis/medico-jovem-no-jaleco-branco-com-estetoscopio-olhando-confiante-em-pe-com-os-bracos-cruzados-sobre-fundo-azul-isolado_141793-9779.jpg?t=st=1746890307~exp=1746893907~hmac=200567344f01ba11a227fff32174b6969aebdfcbd35f9e6d20d13b7488b4a210&w=996"
     },
     {
-        "id": "Id da Médica 3: 0003",
+        "id": "0003",
         "nome": "Dra. Maria Almeida",
         "especialidade": "Oftalmologista",
         "descricao": "Descrição: Especialista em cuidados com os olhos, tratamento de catarata e problemas de visão.",
-        "horario": "Horário: 09h - 13h",
+        "horario": "09h - 13h",
         "foto": "https://img.freepik.com/fotos-premium/positivo-jovem-medico-vestido-com-uniforme-medico-branco-em-pe-no-estudio-com-os-bracos-cruzados_7502-7920.jpg?w=740"
     },
    {
-        "id": "Id da Médica 4: 0004",
+        "id": "0004",
         "nome": "Dra. Jayslane Santos",
         "especialidade": "Ginecologista",
         "descricao": "Descrição: Especialista na saúde da mulher, acompanhamento pré-natal, menopausa e mais.",
-        "horario": "Horário: 09h - 12h",
+        "horario": "09h - 12h",
         "foto": "https://images.pexels.com/photos/5452201/pexels-photo-5452201.jpeg?auto=compress&cs=tinysrgb&h=350"
    },
 ]
